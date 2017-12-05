946f97c25c2121e5977f893638637505ef23b681
tarikbir <7569835+tarikbir@users.noreply.github.com>
2017-12-06 02:03:34 +0300

    commit all

--- TicTacProject/TicTacProject/Program.cs
+++ TicTacProject/TicTacProject/Program.cs
@@ -10,6 +10,8 @@ namespace TicTacProject
     {
         static void Main(string[] args)
         {
+
         }
+
     }
 }
---
+++ TicTacProject/TicTacProject/oyunTahtasi.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacProject
+{
+    class oyunTahtasi
+    {
+        private char[][] oynTahtasi;
+
+        int a;
+
+        public oyunTahtasi()
+        {
+
+        }
+
+        public oyunTahtasi(char[][] oynTahtasi)
+        {
+            this.oynTahtasi.Equals(oynTahtasi);
+            //Start here
+        }
+
+        public char[][] oyunTahtasiniAl()
+        {
+            return oynTahtasi;
+        }
+
+        
+
+
+
+
+
+    }
+}
